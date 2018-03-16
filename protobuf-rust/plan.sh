59ede274c344831b8258c4b120b52288e4e5f269
Elliott Davis <elliott@excellent.io>
2018-03-15 20:22:54 -0500

    Update rust protobuf version
    Signed-off-by: Elliott Davis <elliott@excellent.io>

--- protobuf-rust/plan.sh
+++ protobuf-rust/plan.sh
@@ -1,11 +1,11 @@
 pkg_name=protobuf-rust
 pkg_origin=core
-pkg_version=1.0.18
+pkg_version=1.4.4
 pkg_license=('BSD')
 pkg_source=nosuchfile.tar.gz
 pkg_bin_dirs=(bin)
 pkg_deps=(core/glibc core/gcc)
-pkg_build_deps=(core/rust core/gcc core/cacerts core/protobuf)
+pkg_build_deps=(core/rust core/gcc core/cacerts core/protobuf-cpp)
 
 do_build() {
   env SSL_CERT_FILE=$(pkg_path_for cacerts)/ssl/cert.pem \
